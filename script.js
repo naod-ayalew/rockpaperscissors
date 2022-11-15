1aa5d75eb09f7782b4d91862057302202fd4138a
Naod <nayimam@gmail.com>
2022-11-15 11:52:40 -0800

    returns results and tally to text boxes

--- script.js
+++ script.js
@@ -8,51 +8,71 @@ function getComputerChoice(rps) {   //The randomized choice of the computer sele
     return computerSelection 
 }
 const buttons = document.querySelectorAll('button');
+let pCounter = 0;
+let cCounter = 0;
 
+pScore = document.getElementById('pTally');
+cScore = document.getElementById('cTally');
+
+let results = document.getElementById('roundresults');
+let msg = document.getElementById('message');
  
 buttons.forEach((button) => {
 
  
   button.addEventListener('click', () => {
     console.log(button.id);
-    const cSelect = getComputerChoice(options)
-    const pSelect = button.id
-    let pCounter = 0
-    let cCounter = 0
-    let tieCounter = 0
+    const cSelect = getComputerChoice(options);
+    const pSelect = button.id;
+    
+    
 
     if (pSelect == 'Rock' && cSelect == 'Scissors') {  // This series of if/elseif statements determines the winner and tallies the score of each side
-        alert("You win! " + pSelect + " beats " + cSelect);
+        results.textContent = "You win! \r\n" + pSelect + " beats " + cSelect + ".";
+        console.log(results.textContent)
         pCounter = ++pCounter; 
-        tally.textContent = pCounter;   
+         
+
     }
     else if (pSelect == 'Paper' && cSelect == 'Rock') {
-        alert("You win! " + pSelect + " beats " + cSelect);
+        results.textContent = "You win! \r\n" + pSelect + " beats " + cSelect + ".";
         pCounter = ++pCounter;  
-        tally.textContent = pCounter;   
+         
     }
     else if (pSelect == 'Scissors' && cSelect == 'Paper') {
-        alert("You win! " + pSelect + " beats " + cSelect);
+        results.textContent = "You win! \r\n" + pSelect + " beats " + cSelect + ".";
         pCounter = ++pCounter;  
-        tally.textContent = pCounter;   
+           
     }
     else if (pSelect == 'Rock' && cSelect == 'Paper') {
-        alert("You lose! " + cSelect + " beats " + pSelect);
+        results.textContent = "You lose! \r\n"  + cSelect + " beats " + pSelect + ".";
         cCounter = ++cCounter
     }
     else if (pSelect == 'Paper' && cSelect == 'Scissors') {
-        alert("You lose! " + cSelect + " beats " + pSelect);
+        results.textContent = "You lose! \r\n " + cSelect + " beats " + pSelect + ".";
         cCounter = ++cCounter
     }
     else if (pSelect == 'Scissors' && cSelect == 'Rock') {
-        alert("You lose! " + cSelect + " beats " + pSelect);
+        results.textContent = "You lose! \r\n" + cSelect + " beats " + pSelect + ".";
         cCounter = ++cCounter
     }
-    else if (pSelect == cSelect) {
-    alert("You tied! You both picked " + pSelect);
-    tieCounter = ++tieCounter;
-    } 
+    else if(pSelect == cSelect) {
+        results.textContent = "You tied! \r\n You both picked " + cSelect + ".";
+        
+    }
+    if (pCounter == 5) {
+        msg.textContent = "Congrats. You won! Try again!"
+         
+    }
+    if (cCounter == 5) {
+        msg.textContent = "Shoot! You lost Try again!"
+        
+    }
+    
 
+   
+    pScore.textContent = pCounter
+    cScore.textContent = cCounter
 
      
 
