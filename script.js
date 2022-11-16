4330afcd36b0c0b4d7a0a49096b98be4efa9ef7e
Naod <nayimam@gmail.com>
2022-11-15 20:48:32 -0800

    Worked out the CSS

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const options = ["Rock", "Paper", "Scissors"]
     
 function getComputerChoice(rps) {   //The randomized choice of the computer selection is below
     computerSelection = rps[Math.floor(Math.random()*rps.length)];
-    console.log("comp choose", computerSelection)
+    
     return computerSelection 
 }
 const buttons = document.querySelectorAll('button');
@@ -14,69 +14,228 @@ let cCounter = 0;
 pScore = document.getElementById('pTally');
 cScore = document.getElementById('cTally');
 
-let results = document.getElementById('roundresults');
+let text = document.getElementById('text');
 let msg = document.getElementById('message');
+const resetButton = document.getElementsByClassName('reset');
+rst = resetButton[0]
+ 
+ 
+ 
+ 
  
 buttons.forEach((button) => {
 
- 
   button.addEventListener('click', () => {
-    console.log(button.id);
     const cSelect = getComputerChoice(options);
     const pSelect = button.id;
-    
-    
-
-    if (pSelect == 'Rock' && cSelect == 'Scissors') {  // This series of if/elseif statements determines the winner and tallies the score of each side
-        results.textContent = "You win! \r\n" + pSelect + " beats " + cSelect + ".";
-        console.log(results.textContent)
-        pCounter = ++pCounter; 
-         
-
-    }
-    else if (pSelect == 'Paper' && cSelect == 'Rock') {
-        results.textContent = "You win! \r\n" + pSelect + " beats " + cSelect + ".";
-        pCounter = ++pCounter;  
-         
-    }
-    else if (pSelect == 'Scissors' && cSelect == 'Paper') {
-        results.textContent = "You win! \r\n" + pSelect + " beats " + cSelect + ".";
-        pCounter = ++pCounter;  
-           
-    }
-    else if (pSelect == 'Rock' && cSelect == 'Paper') {
-        results.textContent = "You lose! \r\n"  + cSelect + " beats " + pSelect + ".";
-        cCounter = ++cCounter
-    }
-    else if (pSelect == 'Paper' && cSelect == 'Scissors') {
-        results.textContent = "You lose! \r\n " + cSelect + " beats " + pSelect + ".";
-        cCounter = ++cCounter
-    }
-    else if (pSelect == 'Scissors' && cSelect == 'Rock') {
-        results.textContent = "You lose! \r\n" + cSelect + " beats " + pSelect + ".";
-        cCounter = ++cCounter
-    }
-    else if(pSelect == cSelect) {
-        results.textContent = "You tied! \r\n You both picked " + cSelect + ".";
-        
-    }
-    if (pCounter == 5) {
-        msg.textContent = "Congrats. You won! Try again!"
-         
-    }
-    if (cCounter == 5) {
-        msg.textContent = "Shoot! You lost Try again!"
-        
-    }
-    
-
-   
-    pScore.textContent = pCounter
-    cScore.textContent = cCounter
-
      
+    
+/* This is broken down into various if statements to help make sense of the logic for me
+    While I'm sure this code is pretty inefficient, I will try to break it down for my own
+    sense.
 
-  });
+    If the score for each player is less than 4, then the game plays on as normal. The player's
+    selection triggers the random computer selection, the comparison, and then the results are 
+    printed, and the score is updated.
+
+    If the score for each player is four, then the next winner wins the entire game, the buttons
+    are disabled and they can only press the try again button to refresh the page.
+
+    If the score for one player is four, then if that player wins the next round, they win the game.
+    They get the try again button, and it starts again.
+
+*/
+    if (pCounter < 4 && cCounter < 4){ 
+         
+        if (pSelect == 'Rock' && cSelect == 'Scissors') {  
+        text.textContent = "You win this round!\r\n" + pSelect + " beats " + cSelect + ".";
+            pCounter = ++pCounter; 
+        }
+        else if (pSelect == 'Paper' && cSelect == 'Rock') {
+        text.textContent = "You win this round!\r\n" + pSelect + " beats " + cSelect + ".";
+            pCounter = ++pCounter;      
+        }
+        else if (pSelect == 'Scissors' && cSelect == 'Paper') {
+        text.textContent = "You win this round!\r\n" + pSelect + " beats " + cSelect + ".";
+            pCounter = ++pCounter;      
+        }
+        else if (pSelect == 'Rock' && cSelect == 'Paper') {
+        text.textContent = "You lose this round! \r\n"  + cSelect + " beats " + pSelect + ".";
+            cCounter = ++cCounter
+        }
+        else if (pSelect == 'Paper' && cSelect == 'Scissors') {
+        text.textContent = "You lose this round! \r\n " + cSelect + " beats " + pSelect + ".";
+            cCounter = ++cCounter
+        }
+        else if (pSelect == 'Scissors' && cSelect == 'Rock') {
+        text.textContent = "You lose this round! \r\n" + cSelect + " beats " + pSelect + ".";
+            cCounter = ++cCounter
+        }
+        else if(pSelect == cSelect) {
+        text.textContent = "You tied this round! \r\n You both picked " + cSelect + ".";
+            
+        } 
+        pScore.textContent = pCounter
+        cScore.textContent = cCounter
+         
+
+    }   
+    else if (pCounter == 4 && cCounter == 4){
+        if (pSelect == 'Rock' && cSelect == 'Scissors') {  
+            pCounter = ++pCounter;
+            text.textContent = "Great job! You won!";
+            rst.style.display = 'block';   
+            document.getElementById("Rock").disabled = true;
+            document.getElementById("Paper").disabled = true;
+            document.getElementById("Scissors").disabled = true;
+        }
+        else if (pSelect == 'Paper' && cSelect == 'Rock') {
+            pCounter = ++pCounter;
+            text.textContent = "Great job! You won!";
+            rst.style.display = 'block';      
+            buttonsDisable.forEach(disableButtons)
+            document.getElementById("Rock").disabled = true;
+            document.getElementById("Paper").disabled = true;
+            document.getElementById("Scissors").disabled = true;
+        }
+        else if (pSelect == 'Scissors' && cSelect == 'Paper') {
+            pCounter = ++pCounter;
+            text.textContent = "Great job! You won!";
+            rst.style.display = 'block';      
+            buttonsDisable.forEach(disableButtons)
+            document.getElementById("Rock").disabled = true;
+            document.getElementById("Paper").disabled = true;
+            document.getElementById("Scissors").disabled = true;
+        }
+        else if (pSelect == 'Rock' && cSelect == 'Paper') {
+            cCounter = ++cCounter
+            text.textContent = "Shoot! You lost";
+            rst.style.display = 'block'; 
+            document.getElementById("Rock").disabled = true;
+            document.getElementById("Paper").disabled = true;
+            document.getElementById("Scissors").disabled = true;
+        }
+        else if (pSelect == 'Paper' && cSelect == 'Scissors') {
+            cCounter = ++cCounter
+            text.textContent = "Shoot! You lost";
+            rst.style.display = 'block'; 
+            document.getElementById("Rock").disabled = true;
+            document.getElementById("Paper").disabled = true;
+            document.getElementById("Scissors").disabled = true;
+        }
+        else if (pSelect == 'Scissors' && cSelect == 'Rock') {
+            cCounter = ++cCounter
+            text.textContent = "Shoot! You lost";
+            rst.style.display = 'block';   
+            document.getElementById("Rock").disabled = true;
+            document.getElementById("Paper").disabled = true;
+            document.getElementById("Scissors").disabled = true;
+        }
+        else if(pSelect == cSelect) {
+        text.textContent = "You tied this round! \r\n You both picked " + cSelect + ".";
+            
+        } 
+        pScore.textContent = pCounter
+        cScore.textContent = cCounter
+        }
+    
+    else if  (pCounter == 4) {
+
+        if (pSelect == 'Rock' && cSelect == 'Scissors') {  
+                pCounter = ++pCounter;
+                text.textContent = "Great job! You won!";
+                rst.style.display = 'block';   
+                document.getElementById("Rock").disabled = true;
+                document.getElementById("Paper").disabled = true;
+                document.getElementById("Scissors").disabled = true;
+            }
+            else if (pSelect == 'Paper' && cSelect == 'Rock') {
+                pCounter = ++pCounter;
+                text.textContent = "Great job! You won!";
+                rst.style.display = 'block';      
+                 
+                document.getElementById("Rock").disabled = true;
+                document.getElementById("Paper").disabled = true;
+                document.getElementById("Scissors").disabled = true;
+            }
+            else if (pSelect == 'Scissors' && cSelect == 'Paper') {
+                pCounter = ++pCounter;
+                text.textContent = "Great job! You won!";
+                rst.style.display = 'block';      
+                 
+                document.getElementById("Rock").disabled = true;
+                document.getElementById("Paper").disabled = true;
+                document.getElementById("Scissors").disabled = true;
+            }
+            else if (pSelect == 'Rock' && cSelect == 'Paper') {
+            text.textContent = "You lose this round! \r\n"  + cSelect + " beats " + pSelect + ".";
+                cCounter = ++cCounter
+            }
+            else if (pSelect == 'Paper' && cSelect == 'Scissors') {
+            text.textContent = "You lose this round! \r\n " + cSelect + " beats " + pSelect + ".";
+                cCounter = ++cCounter
+            }
+            else if (pSelect == 'Scissors' && cSelect == 'Rock') {
+            text.textContent = "You lose this round! \r\n" + cSelect + " beats " + pSelect + ".";
+                cCounter = ++cCounter
+            }
+            else if(pSelect == cSelect) {
+            text.textContent = "You tied this round! \r\n You both picked " + cSelect + ".";
+                
+            } 
+            pScore.textContent = pCounter
+            cScore.textContent = cCounter
+          
+        }
+        else if (cCounter = 4) {
+            if (pSelect == 'Rock' && cSelect == 'Scissors') {  
+                    text.textContent = "You win this round!\r\n" + pSelect + " beats " + cSelect + ".";
+                    pCounter = ++pCounter; 
+
+
+                }
+                else if (pSelect == 'Paper' && cSelect == 'Rock') {
+                text.textContent = "You win this round!\r\n" + pSelect + " beats " + cSelect + ".";
+                    pCounter = ++pCounter;      
+                }
+                else if (pSelect == 'Scissors' && cSelect == 'Paper') {
+                text.textContent = "You win this round!\r\n" + pSelect + " beats " + cSelect + ".";
+                    pCounter = ++pCounter;      
+                }
+                else if (pSelect == 'Rock' && cSelect == 'Paper') {
+                    cCounter = ++cCounter
+                    text.textContent = "Shoot! You lost";
+                    rst.style.display = 'block'; 
+                    document.getElementById("Rock").disabled = true;
+                    document.getElementById("Paper").disabled = true;
+                    document.getElementById("Scissors").disabled = true;
+                }
+                else if (pSelect == 'Paper' && cSelect == 'Scissors') {
+                    cCounter = ++cCounter
+                    text.textContent = "Shoot! You lost";
+                    rst.style.display = 'block'; 
+                    document.getElementById("Rock").disabled = true;
+                    document.getElementById("Paper").disabled = true;
+                    document.getElementById("Scissors").disabled = true;
+                }
+                else if (pSelect == 'Scissors' && cSelect == 'Rock') {
+                    cCounter = ++cCounter
+                    text.textContent = "Shoot! You lost";
+                    rst.style.display = 'block';   
+                    document.getElementById("Rock").disabled = true;
+                    document.getElementById("Paper").disabled = true;
+                    document.getElementById("Scissors").disabled = true;
+                }
+                else if(pSelect == cSelect) {
+                text.textContent = "You tied this round! \r\n You both picked " + cSelect + ".";
+                    
+                } 
+                pScore.textContent = pCounter
+                cScore.textContent = cCounter
+                }
+
+                
+    });
 });
 
 
